1611ddf938d4dcaedec4cbc558feb25050654b13
Mark-Ball <markball111@gmail.com>
2020-01-15 14:18:38 +1100

    implemented disappearing input field on submit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import NameHeader from './components/NameHeader';
 
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { username: '' };
-      }
-      myChangeHandler = (event) => {
-        this.setState({username: event.target.value});
-      }
-      mySubmitHandler = (event) => {
-        event.preventDefault();
-      }
-      render() {
+    render() {
         return (
-            <form onSubmit={this.mySubmitHandler}>
-            <h1>Hello {this.state.username}</h1>
-            <p>Enter your name, and submit:</p>
-            <input
-              type='text'
-              onChange={this.myChangeHandler}
-            />
-            <input
-              type='submit'
-            />
-            
-            </form>
-          );
-        }
-      }
-    
-ReactDOM.render(<App />, document.getElementById('root'));
+            <>
+                <NameHeader />
+            </>
+        )
+    }
+}
 
 export default App;
